42e0c994dfaafb0909317ed099fa113c3fbd638c
Wolfgang Schwanghart <schwanghart@gmail.com>
2023-06-14 13:06:42 +0200

    update Contents.m

--- Contents.m
+++ Contents.m
@@ -44,82 +44,91 @@
 %
 % GRIDobj methods
 %
-%     GRIDOBJ           : Create instance of a GRIDobj
-%     GRIDOBJ2ASCII     : write/export GRIDobj to ESRI ArcGIS ASCII file
-%     GRIDOBJ2GEOTIFF   : Exports an instance of GRIDobj to a geotiff file
-%     GRIDOBJ2IM        : GRIDOBJ2IM Create image from GRIDobj
-%     GRIDOBJ2MAT       : convert GRIDobj to matrix and coordinate vectors
-%     GRIDOBJ2PM        : combine several GRIDobj into a predictor matrix
-%     GRIDOBJ2POLYGON   : Conversion from drainage basin grid to polygon or polyline
-%     ACV               : Anisotropic coefficient of variation (ACV) 
-%     AGGREGATE         : resampling a GRIDobj using aggregation
-%     ARCSLOPE          : mean gradient from a digital elevation model sensu ArcGIS
-%     ASPECT            : angle of exposition from a digital elevation model (GRIDobj)
-%     CASTSHADOW        : cast shadow
-%     CELLAREA          : calculate cell areas of a GRIDobj in geographic coordinate system
-%     CLIP              : clip a GRIDobj with a polygon or another GRIDobj
-%     CONTOUR           : contour plot of an instance of GRIDobj
-%     COORD2IND         : convert x and y coordinates to linear index
-%     COORD2SUB         : convert x and y coordinates to subscripts into a GRIDobj
-%     CREATEMASK        : create a binary mask using polygon mapping
-%     CROP              : crop an instance of GRIDobj with axis-aligned minimum bounding box
-%     CURVATURE         : 8-connected neighborhood curvature of a digital elevation model 
-%     DEMAREA           : Calculate the corrected surface area of a DEM
-%     DEMPROFILE        : get profile along path
-%     DILATE            : morphological dilation
-%     DIST2CURVE        : labels pixels in a GRIDobj by their directed distance to a curved line
-%     DIST2LINE         : labels pixels in a GRIDobj by their distance to a straight line
-%     DISTANCE          : distance transform
-%     ELEVATEMINIMA     : elevate regional minima in a DEM to their lowest neighbor
-%     ERODE             : morphological erosion
-%     EXCESSTOPOGRAPHY  : reconstruct surface with threshold-slope surface
-%     FILLSINKS         : fill/remove pits, sinks or topographic depressions
-%     FILTER            : 2D-filtering of DEMs with different kernels 
-%     FIND              : Find indices of nonzero elements in GRIDobj
-%     FINDCOORD         : Find coordinates of nonzero elements in GRIDobj
-%     GETCOORDINATES    : get coordinate vectors of an instance of GRIDobj
-%     GETEXTENT         : return extent of a GRIDobj
-%     GETOUTLINE        : get or plot extent of GRIDobj
-%     GRADIENT8         : 8-connected neighborhood gradient of a digital elevation model
-%     GRIDDEDCONTOUR    : plot contours on grid
-%     HILLSHADE         : create hillshading from a digital elevation model (GRIDobj)
-%     HYPSCURVE         : plot hypsometric curve of a digital elevation model
-%     IDENTIFYFLATS     : identify flat terrain in a digital elevation model
-%     IMAGESC           : Scale data in GRIDobj and display as image object
-%     IMAGESCHS         : plot hillshade image with overlay
-%     IND2COORD         : convert linear index to x and y coordinates
-%     INFO              : detailed information on GRIDobj instance
-%     INPAINTNANS       : interpolate missing values in a grid (GRIDobj)
-%     INTERP            : interpolate to query locations
-%     INTERP2GRIDOBJ    : Interpolate scattered data to GRIDobj
-%     ISNAN             : returns array elements that are NaNs as logical grid
-%     KSDENSITY         : kernel density estimator for GRIDobj
-%     LATLON            : returns grid lines of latitudes and longitudes
-%     LINE2GRIDOBJ      : convert line to a grid
-%     LOCALTOPOGRAPHY   : Local topography
-%     MEASURE           : take interactive measurements along a polyline
-%     MINMAXNORM        : min-max normalization with optional percent clipping
-%     MPOWER            : overloaded power for GRIDobj
-%     MRDIVIDE          : overloaded right division for GRIDobj
-%     MTIMES            : overloaded multiplication for GRIDobj
-%     PAD               : add or remove a border of pixels around a GRIDobj
-%     POLYGON2GRIDOBJ   : convert polygon to a grid
-%     POSTPROCFLATS     : postprocess flat terrain for visualization purpose
-%     PRCCLIP           : percentile clipping
-%     PROJECT           : transforms a GRIDobj between projected coordinate systems
-%     RECLABEL          : labels GRIDobj by rectangular fields
-%     RECLASSIFY        : generate univariate class intervals for an instance of GRIDobj
-%     REPROJECT2UTM     : Reproject DEM with WGS84 coordinate system to UTM-WGS84 
-%     RESAMPLE          : change spatial resolution of a GRIDobj
-%     ROUGHNESS         : terrain ruggedness, position and roughness indices of DEMs
-%     SHUFFLELABEL      : shufflelabel randomly relabels a label matrix
-%     SNAP2STREAM       : snap gauges or pour points to stream raster
-%     SUB2COORD         : convert subscripts to x and y coordinates
-%     SURF              : surface plot for GRIDobj
-%     TANAKACONTOUR     : Relief depiction using Tanaka contours
-%     TOPOSHIELDING     : topographic shielding from cosmic rays
-%     VALIDATEALIGNMENT : validates whether instances of GRIDobj are spatially aligned
-%     ZSCORE            : standardized z-scores for GRIDobj
+%     GRIDOBJ            : Create instance of a GRIDobj
+%     GRIDOBJ2ASCII      : write/export GRIDobj to ESRI ArcGIS ASCII file
+%     GRIDOBJ2GEOTIFF    : Exports an instance of GRIDobj to a geotiff file
+%     GRIDOBJ2IM         : GRIDOBJ2IM Create image from GRIDobj
+%     GRIDOBJ2MAT        : convert GRIDobj to matrix and coordinate vectors
+%     GRIDOBJ2PM         : combine several GRIDobj into a predictor matrix
+%     GRIDOBJ2POLYGON    : Conversion from drainage basin grid to polygon or polyline
+%     GRIDOBJ2RGB        : Convert GRIDobj to RGB image
+%     ACV                : Anisotropic coefficient of variation (ACV) 
+%     AGGREGATE          : resampling a GRIDobj using aggregation
+%     ARCSLOPE           : mean gradient from a digital elevation model sensu ArcGIS
+%     ASPECT             : angle of exposition from a digital elevation model (GRIDobj)
+%     CASTSHADOW         : cast shadow
+%     CELLAREA           : calculate cell areas of a GRIDobj in geographic coordinate system
+%     CLIP               : clip a GRIDobj with a polygon or another GRIDobj
+%     CONTOUR            : contour plot of an instance of GRIDobj
+%     COORD2IND          : convert x and y coordinates to linear index
+%     COORD2SUB          : convert x and y coordinates to subscripts into a GRIDobj
+%     CREATEMASK         : create a binary mask using polygon mapping
+%     CROP               : crop an instance of GRIDobj with axis-aligned minimum bounding box
+%     CURVATURE          : 8-connected neighborhood curvature of a digital elevation model 
+%     DEMAREA            : Calculate the corrected surface area of a DEM
+%     DEMPROFILE         : get profile along path
+%     DIFFUSION          : Solve the diffusion equation
+%     DILATE             : morphological dilation
+%     DIST2CURVE         : labels pixels in a GRIDobj by their directed distance to a curved line
+%     DIST2LINE          : labels pixels in a GRIDobj by their distance to a straight line
+%     DISTANCE           : distance transform
+%     ELEVATEMINIMA      : elevate regional minima in a DEM to their lowest neighbor
+%     ERODE              : morphological erosion
+%     EVANSSLOPE         : Calculate surface slope using Evans method
+%     EXCESSTOPOGRAPHY   : reconstruct surface with threshold-slope surface
+%     FILLSINKS          : fill/remove pits, sinks or topographic depressions
+%     FILTER             : 2D-filtering of DEMs with different kernels 
+%     FIND               : Find indices of nonzero elements in GRIDobj
+%     FINDCOORD          : Find coordinates of nonzero elements in GRIDobj
+%     GETCOORDINATES     : get coordinate vectors of an instance of GRIDobj
+%     GETEXTENT          : return extent of a GRIDobj
+%     GETOUTLINE         : get or plot extent of GRIDobj
+%     GRADIENT8          : 8-connected neighborhood gradient of a digital elevation model
+%     GRIDDEDCONTOUR     : plot contours on grid
+%     HILLSHADE          : create hillshading from a digital elevation model (GRIDobj)
+%     HISTOGRAM          : Plot frequency distribution of values in GRIDobj
+%     HYPSCURVE          : plot hypsometric curve of a digital elevation model
+%     IDENTIFYFLATS      : identify flat terrain in a digital elevation model
+%     IMAGESC            : Scale data in GRIDobj and display as image object
+%     IMAGESCHS          : plot hillshade image with overlay
+%     IND2COORD          : convert linear index to x and y coordinates
+%     INFO               : detailed information on GRIDobj instance
+%     INPAINTNANS        : Interpolate or fill missing values in a grid (GRIDobj)
+%     INPAINTNANSORDFILT : DEM inpainting using 2D order-statistics filtering
+%     INTERP             : interpolate to query locations
+%     INTERP2GRIDOBJ     : Interpolate scattered data to GRIDobj
+%     INTERPWITHBARRIERS : Laplace interplation with barriers
+%     ISNAN              : returns array elements that are NaNs as logical grid
+%     KSDENSITY          : kernel density estimator for GRIDobj
+%     LATLON             : returns grid lines of latitudes and longitudes
+%     LINE2GRIDOBJ       : convert line to a grid
+%     LOCALTOPOGRAPHY    : Local topography
+%     MEASURE            : take interactive measurements along a polyline
+%     MINMAXNORM         : min-max normalization with optional percent clipping
+%     MPOWER             : overloaded power for GRIDobj
+%     MRDIVIDE           : overloaded right division for GRIDobj
+%     MTIMES             : overloaded multiplication for GRIDobj
+%     PAD                : add or remove a border of pixels around a GRIDobj
+%     POLYGON2GRIDOBJ    : convert polygon to a grid
+%     POSTPROCFLATS      : postprocess flat terrain for visualization purpose
+%     PRCCLIP            : percentile clipping
+%     PROJECT            : transforms a GRIDobj between projected coordinate systems
+%     PROMINENCE         : Calculate the prominence of mountain peaks
+%     RAND               : Compute a GRIDobj with random numbers
+%     RANDOMSAMPLE       : Uniform random sampling of a GRIDobj
+%     RECLABEL           : labels GRIDobj by rectangular fields
+%     RECLASSIFY         : generate univariate class intervals for an instance of GRIDobj
+%     REPROJECT2UTM      : Reproject DEM with WGS84 coordinate system to UTM-WGS84 
+%     RESAMPLE           : change spatial resolution of a GRIDobj
+%     ROUGHNESS          : terrain ruggedness, position and roughness indices of DEMs
+%     SHUFFLELABEL       : shufflelabel randomly relabels a label matrix
+%     SNAP2STREAM        : snap gauges or pour points to stream raster
+%     SUB2COORD          : convert subscripts to x and y coordinates
+%     SURF               : surface plot for GRIDobj
+%     TANAKACONTOUR      : Relief depiction using Tanaka contours
+%     TOPOSHIELDING      : topographic shielding from cosmic rays
+%     VALIDATEALIGNMENT  : validates whether instances of GRIDobj are spatially aligned
+%     ZSCORE             : standardized z-scores for GRIDobj
 % 
 % FLOWobj methods
 % 
@@ -128,6 +137,7 @@
 %     FLOWOBJ2M           : convert instance of FLOWobj to flow direction matrix 
 %     FLOWOBJ2CELL        : return cell array of FLOWobjs for individual drainage basins
 %     CROP                : crop an instance of FLOWobj
+%     DBASYMMETRY         : Drainage basin asymmetry
 %     DBENTROPY           : entropy of drainage basin delineation
 %     DEPENDENCEMAP       : upslope area for specific locations in a DEM
 %     DRAINAGEBASINS      : drainage basin delineation/catchments
@@ -143,20 +153,25 @@
 %     IMPOSEMIN           : minima imposition (carving) along drainage network
 %     IND2COORD           : convert linear index to x and y coordinates
 %     INFLUENCEMAP        : downslope area for specific locations in a digital elevation model
+%     ISGEOGRAPHIC        : Determines if FLOWobj has a geographic coordinate system
 %     ISMULTI             : check if FD is multi or single flow direction
 %     MAPFROMNAL          : map values from node-attribute list to nearest upstream grid
+%     MELTONRUGGEDNESS    : Melton ruggedness
 %     MULTI2SINGLE        : converts multiple to single flow direction
 %     MULTI_NORMALIZE     : create flow direction object
 %     MULTI_WEIGHTS       : create flow direction object
+%     PLOTDBFRINGE        : Plot semitransparent fringe around each drainage basin
 %     QUANTCARVE          : quantile carving
 %     RANDOMIZE           : randomize multiple flow directions
 %     SAVEOBJ             : create flow direction object
+%     SLOPEAREATOOL       : Interactively create slope area plots and fit power laws
 %     STREAMORDER         : calculates a stream order GRIDobj from FLOWobj
 %     STREAMPOI           : stream points of interest
+%     TFACTOR             : Transverse topographic symmetry (T-)factor
 %     UPDATETOPOSORT      : update topological sorting
-%     UPSLOPESTATS        : upslope statistics of a variable based on the flow direction matrix
+%     UPSLOPESTATS        : upslope statistics computed in flow directions
 %     VALIDATEALIGNMENT   : validates whether instances of FLOWobj and GRIDobj are spatially aligned
-%     VERTDISTANCE2STREAM : vertical distance to streams
+%     VERTDISTANCE2STREAM : vertical distance to streams 
 % 
 % STREAMobj methods
 % 
@@ -165,10 +180,12 @@
 %     STREAMOBJ2SWATHOBJ  : Create swath profile (SWATHobj) from stream network
 %     STREAMOBJ2XY        : convert instance of STREAMobj to NaN-separated X and Y coordinates
 %     STREAMOBJ2CELL      : convert instance of STREAMobj to cell array of stream objects
+%     STREAMOBJ2KML       : Convert STREAMobj to kml (Google Earth)
 %     STREAMOBJ2LATLON    : convert instance of STREAMobj to NaN-separated geographic coordinates
 %     STREAMOBJ2MAPSTRUCT : convert instance of STREAMobj to mapstruct
 %     STREAMOBJ2SHAPE     : Convert STREAMobj to geoshape or mapshape
-%     AGGREGATE           : aggregating values of reaches
+%     AGGREGATE           : Summarizing values within segments of the stream network
+%     BINARIZE            : Make a stream network a strictly binary tree
 %     CHIPLOT             : CHI analysis for bedrock river analysis
 %     CHITRANSFORM        : Coordinate transformation using the integral approach
 %     CLEAN               : Create stream object (STREAMobj)
@@ -181,9 +198,13 @@
 %     CUMTRAPZ            : Cumulative trapezoidal numerical integration along a stream network
 %     CURVATURE           : curvature or 2nd derivative of a STREAMobj
 %     DENSIFY             : Increase number of vertices in stream network using splines
+%     DIFF                : Differences between adjacent pixels in a stream network
 %     DISTANCE            : return node attribute list with distances along the stream network
 %     DRAINAGEDENSITY     : drainage density of a stream network
+%     EXTEND2DIVIDE       : Grow STREAMobj upstream to extend to the divide
 %     EXTRACTCONNCOMPS    : interactive stream network selection
+%     FASTSCAPE           : Simulation of the stream power incision model
+%     GETLOCATION         : Get locations along a stream network
 %     GETNAL              : get node-attribute list
 %     GETVALUE            : retrieve value from node-attribute list
 %     GRADIENT            : along-stream gradient
@@ -193,20 +214,26 @@
 %     INFO                : meta information about STREAMobj
 %     INPAINTNANS         : inpaint missing values (nans) in a node attribute list
 %     INTERP              : interpolate data on STREAMobj (single river only)
+%     INTERPSMOOTH        : Interpolate and smooth values measured along a stream network
 %     INTERSECT           : intersect different instances of STREAMobj 
 %     INTERSECTLOCS       : Derive locations where two STREAMobj start to have a common network
-%     ISGEOGRAPHIC        : Determines if STREAMobj S has a geographic coordinate system
+%     ISEMPTY             : Determine whether a STREAMobj is empty
+%     ISEQUAL             : Determine whether two STREAMobjs are equal
+%     ISGEOGRAPHIC        : Determines if STREAMobj has a geographic coordinate system
 %     ISNAL               : test whether a vector is a node attribute list of a STREAMobj
 %     ISSUBGRAPH          : Create stream object (STREAMobj)
+%     ISTRUNK             : Determines whether STREAMobj consists of trunk streams
 %     KLARGESTCONNCOMPS   : retain k largest connected components in an instance of STREAMobj
 %     KNICKPOINTFINDER    : find knickpoints in river profiles
 %     KSN                 : normalized steepness index
 %     LABELREACH          : create node-attribute list with labelled reaches
+%     LOESSKSN            : Loess-smoothed river steepness
 %     MAPLATERAL          : map values of regions adjacent to streams to stream network
 %     MCHI                : gradient of stream profile in chi space (M_chi)
 %     MEANUPSTREAM        : mean (weighted) upstream  values
 %     MINCOSTHYDROCON     : minimum cost hydrological conditioning
 %     MNOPTIM             : Bayesian optimization of the mn ratio
+%     MNOPTIMVAR          : optimize the mn ratio using minimum variance method
 %     MODIFY              : modify instance of STREAMobj to meet user-defined criteria
 %     NAL2NAL             : map one node-attribute list to another 
 %     NETDIST             : distance transform on a stream network
@@ -220,7 +247,7 @@
 %     PLOTDZSHADED        : plot upstream distance version elevation of a stream network
 %     PLOTSEGMENTGEOMETRY : Plot segment geometry obtained from the function networksegment
 %     PLOTSTREAMORDER     : calculate stream order from STREAMobj
-%     PSITRANSFORM        : Parse Inputs
+%     PSITRANSFORM        : Psi-transformation of river network elevations
 %     QUANTCARVE          : quantile carving
 %     RANDLOCS            : Random locations along the stream network
 %     REMOVEEDGEEFFECTS   : remove potential edge effects
